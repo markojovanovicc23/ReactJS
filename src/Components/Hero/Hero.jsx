494f3ce5864d1f14d60e3740b9c0a7594d5cd833
markojovanovicc23 <145758273+markojovanovicc23@users.noreply.github.com>
2023-11-11 21:58:04 +0100

    Update na About sekciji
    desinger nije cawa liche

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Hero.css';
 import heroslika from '../Hero/slika.jpg';
+import unutra from '../Hero/unutra.jpg';
 
 function Hero() {
   return (
@@ -16,6 +17,18 @@ function Hero() {
         </div>
       </div>
       <h2>About <span>Us</span></h2>
+      <div className='img-unutra'>
+      <img src={unutra} />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br/> Placeat, veritatis. Porro cupiditate incidunt, corporis tempora asperiores iusto aut consequuntur, qui, quis temporibus et natus dolorem quibusdam. <br/> Praesentium, repellat non?</p>
+      <div className='lista'>
+        <ul>
+        <li> 250 m2 <br/> <span>Total Flat Space</span> </li>
+        <li> Contract </li>
+        <li></li>
+        <li></li>
+      </ul>
+      </div>
+      </div>
     </div>
   )
 }
